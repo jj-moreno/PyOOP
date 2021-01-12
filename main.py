18a47224f7a11f8e9e2bd46e1b8c37500b0e67be
Jose Moreno <jomoreno@teksystems.com>
2021-01-12 14:21:38 -0600

    formatted str for Segment

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ class Segment(object):
         self.end_point = end_point
 
     def __str__(self):
-        return f"start_point: ({self.start_point.x}, { self.start_point.y})\n" \
-               f"end_point: ({self.end_point.x}, { self.end_point.y})\n"
+        return f"start_point: ({self.start_point.x}, {self.start_point.y})\n" \
+               f"end_point: ({self.end_point.x}, {self.end_point.y})\n"
 
     def length(self):
-        return math.sqrt(abs(self.end_point.x - self.start_point.x)**2
-                         + abs(self.end_point.y - self.start_point.y)**2)
+        return math.sqrt(abs(self.end_point.x - self.start_point.x) ** 2
+                         + abs(self.end_point.y - self.start_point.y) ** 2)
 
 
 class Point(object):
