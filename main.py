37f7661412bb021bafc4a3987c377ee5c54298e2
unknown <moreno7046@hotmail.com>
2021-01-15 20:12:11 -0600

    minor cosmetic changes, method and attribute name changes

--- main.py
+++ main.py
@@ -26,14 +26,13 @@ class Segment(object):
 
 
 class Drunk(object):
-
     def __init__(self, start_point):
         self.drunk_loc = start_point
         self.possibilites = ['N', 'S', 'E', 'W']
 
-    def randmove(self, point):
-        new_x = point.x
-        new_y = point.y
+    def randmove(self, new_point):
+        new_x = new_point.x
+        new_y = new_point.y
         rand_move = random.choice(self.possibilites)
         if rand_move == self.possibilites[0]:
             new_y = new_y + 1
@@ -50,12 +49,11 @@ class Drunk(object):
 
 
 class Field(object):
-
     def __init__(self, drunk):
         self.start_loc = drunk.drunk_loc
 
-    def getdistance(self, start_loc, new_loc):
-        distance = Segment(start_loc, new_loc).length()
+    def getdistance(self, new_loc):
+        distance = Segment(self.start_loc, new_loc).length()
         return distance
 
     def __getattribute__(self, start_loc):
@@ -63,28 +61,27 @@ class Field(object):
 
 
 class Trial(object):
-
-    def __init__(self, time, start_point=Point(0, 0)):
+    def __init__(self, alloted_time, start_point=Point(0, 0)):
         self.drunk = Drunk(start_point)
         self.field = Field(self.drunk)
-        self.time = time
+        self.alloted_time = alloted_time
         self.distances = []
 
-    def perform_trial(self):
+    def performtrial(self):
         current_time = 0
-        start_loc = self.field.start_loc
-        while current_time < self.time:
+        while current_time < self.alloted_time:
             self.drunk.randmove(self.drunk.drunk_loc)
             drunk_loc = self.drunk.drunk_loc
-            distancetravel = self.field.getdistance(start_loc, drunk_loc)
-            self.distances.append(distancetravel)
+            distance_traveled = self.field.getdistance(drunk_loc)
+            self.distances.append(distance_traveled)
             current_time += 1
 
-    def print_result(self):
+    def printresult(self):
         print(self.distances)
 
 
 if __name__ == '__main__':
-    trial = Trial(5)
-    trial.perform_trial()
-    trial.print_result()
+    time_in_secs = 5
+    trial = Trial(time_in_secs)
+    trial.performtrial()
+    trial.printresult()
